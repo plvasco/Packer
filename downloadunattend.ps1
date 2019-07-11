9224287125144d1eb9a322a00e3c685864fa2afc
plvasco <35873800+plvasco@users.noreply.github.com>
2019-07-11 13:20:39 -0500

    Update downloadunattend.ps1

--- downloadunattend.ps1
+++ downloadunattend.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri "https://piettostiggpos.s3-us-gov-east-1.amazonaws.com/Unattend.xml" -OutFile "C:\ProgramData\Amazon\EC2-Windows\Launch\Sysprep\Unattend.xml"
+Invoke-WebRequest -Uri "https://piettostiggpos.s3-us-gov-east-1.amazonaws.com/WindowsUpdate.ps1" -OutFile "C:\WindowsUpdate.ps1"
 Invoke-WebRequest -Uri "https://piettostiggpos.s3-us-gov-east-1.amazonaws.com/VA-Internal-S2-RCA1-v1.cer" -OutFile "C:\VA-Internal-S2-RCA1-v1.cer"
