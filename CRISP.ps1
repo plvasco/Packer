0e73d431e64b2bde180f75f7f57b17f22d978a6c
plvasco <35873800+plvasco@users.noreply.github.com>
2019-07-15 12:50:40 -0500

    Add files via upload

---
+++ CRISP.ps1
@@ -0,0 +1,14 @@
+﻿Expand-Archive -Path C:\s3-downloads\Chocopkg4Download.zip -DestinationPath C:\s3-downloads\Chocopkg4Download
+
+cd C:\s3-downloads\Chocopkg4Download\Chocopackages\bigfixagent
+choco install -y bigfixagent
+cd C:\s3-downloads\Chocopkg4Download\Chocopackages\mcafeeagent
+choco install -y mcafeeagent
+cd C:\s3-downloads\Chocopkg4Download\Chocopackages\safenet
+choco install -y safenet
+cd C:\s3-downloads\Chocopkg4Download\Chocopackages\encase
+choco install -y encase
+cd C:\s3-downloads\Chocopkg4Download\Chocopackages\wincollect
+choco install -y wincollect
+choco install -y splunk
+choco install -y aws cli
